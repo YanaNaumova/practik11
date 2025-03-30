73f1439e07a1a893358b9ddcfa2949245753c433
YanaNaumova <naumovayana87@gmail.com>
2025-03-30 11:10:59 +0200

    add navigate

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import Users from "./pages/Users";
 import Navbars from "./components/Navbars";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import UserProfile from "./pages/UserProfile";
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
       <Navbars />
       <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<Home />} />
         <Route path="/users" element={<Users />} />
         <Route
